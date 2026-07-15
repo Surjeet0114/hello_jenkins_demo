b950d8d1dd1e9af67ce5940164f889978e9707fe
Surjeet14 <su140105@gmail.com>
2026-07-15 21:45:39 +0530

    first commit

---
+++ src/main/java/com/surjeet/hello_jenkins_demo/HelloJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.surjeet.hello_jenkins_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJenkinsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surjeet/hello_jenkins_demo/HelloJenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surjeet.hello_jenkins_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJenkinsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
